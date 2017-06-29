49023d76c49373c9df23748554f67f03d88129bc
issm <issmxx@gmail.com>
2017-06-29 18:32:45 +0900

    `rediq-client`, `rediq-watcher`: add test about connection to Redis.

--- test/rediq-client/constructor-test.js
+++ test/rediq-client/constructor-test.js
@@ -10,5 +10,36 @@ describe('rediq-client', () => {
 
             done()
         })
+
+        describe('connection to Redis', () => {
+            let client = new Client()
+
+            let key = 'rediq-client:foobar'
+            let val = 'foo-bar-baz'
+
+            it('セットできる', (done) => {
+                client.redis.setAsync(key, val, 'EX', 20)
+                    .then((res) => {
+                        should.exists(res)
+                        done()
+                    })
+                    .catch((err) => {
+                        should.fail()
+                        done()
+                    })
+            })
+
+            it('セットした情報を取得できる', (done) => {
+                client.redis.getAsync(key)
+                    .then((res) => {
+                        res.should.be.equal(val)
+                        done()
+                    })
+                    .catch((err) => {
+                        should.fail()
+                        done()
+                    })
+            })
+        })
     })
 })
--- test/rediq-watcher/constructor-test.js
+++ test/rediq-watcher/constructor-test.js
@@ -10,5 +10,35 @@ describe('rediq-watcher', () => {
 
             done()
         })
+
+        describe('connection to Redis', () => {
+            let watcher = new Watcher()
+
+            let key = 'rediq-watcher:foobar'
+            let val = 'foo-bar-baz'
+
+            it('セットできる', (done) => {
+                watcher.redis.setAsync(key, val, 'EX', 20)
+                    .then((res) => {
+                        should.exists(res)
+                        done()
+                    })
+                    .catch((err) => {
+                        should.fail()
+                        done()
+                    })
+            })
+
+            it('セットした情報を取得できる', (done) => {
+                watcher.redis.getAsync(key)
+                    .then((res) => {
+                        res.should.be.equal(val)
+                        done()
+                    })
+                    .catch((err) => {
+                        done()
+                    })
+            })
+        })
     })
 })

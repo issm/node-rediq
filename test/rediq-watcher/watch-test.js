f50167c05725e11f8a9b5f03b86122a0fc65f0da
issm <issmxx@gmail.com>
2017-08-01 18:56:18 +0900

    `rediq-watcher`: `#watch`: 基本的な部分を実装．テストは書けていない．

---
+++ test/rediq-watcher/watch-test.js
@@ -0,0 +1,10 @@
+const should = require('should')
+const {Watcher, Worker} = require('rediq')
+
+describe('rediq-watcher', () => {
+    describe('#watch', () => {
+        it.skip('TODO', (done) => {
+            done()
+        })
+    })
+})

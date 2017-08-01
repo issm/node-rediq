c9afc56308bcfb2089d1584205ed01099c410d8f
issm <issmxx@gmail.com>
2017-08-01 15:55:20 +0900

    `rediq-watcher`: `#constructor`: option `target` is required.

--- test/rediq-watcher/constructor-test.js
+++ test/rediq-watcher/constructor-test.js
@@ -2,17 +2,48 @@ const should = require('should')
 const {Watcher} = require('rediq')
 
 describe('rediq-watcher', () => {
+    describe('constructor options', () => {
+        describe('`target`', () => {
+            it('missing -> error', (done) => {
+                try {
+                    let opts = {}
+                    let watcher = new Watcher(opts)
+                    should.fail()
+                } catch(err) {
+                    should.exist(err)
+                } finally {
+                    done()
+                }
+            })
+
+            it('specified -> ok', (done) => {
+                try {
+                    let opts = {}
+                    let watcher = new Watcher(opts)
+                    should.exist(watcher)
+                } catch(err) {
+                    should.fail()
+                } finally {
+                    done()
+                }
+            })
+        })
+    })
+
     describe('constructor', () => {
         it('properties', (done) => {
-            let client = new Watcher()
+            let opts = { target: 'foobar' }
+            let watcher = new Watcher(opts)
 
-            client.should.have.properties('redis').which.is.a.Object()
+            watcher.should.have.property('redis').which.is.a.Object()
+            watcher.should.have.property('target').which.is.equal(opts.target)
 
             done()
         })
 
         describe('connection to Redis', () => {
-            let watcher = new Watcher()
+            let opts = { target: 'foobar' }
+            let watcher = new Watcher(opts)
 
             let key = 'rediq-watcher:foobar'
             let val = 'foo-bar-baz'

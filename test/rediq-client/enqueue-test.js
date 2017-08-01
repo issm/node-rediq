a0097b2096dbec4b40f7d8ef30da20c88003dd3f
issm <issmxx@gmail.com>
2017-08-01 19:53:57 +0900

    `rediq-client`: `#enqueue`: 基本的な部分を実装．テストは書けていない．

---
+++ test/rediq-client/enqueue-test.js
@@ -0,0 +1,10 @@
+const should = require('should')
+const {Client} = require('rediq')
+
+describe('rediq-client', () => {
+    describe('#enqueue', () => {
+        it.skip('TODO', (done) => {
+            done()
+        })
+    })
+})
